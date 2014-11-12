ef2c732a226edaef984bc458a07dde47c7e8922c
nancy.taylor <ntaylor120@gmail.com>
2014-11-12 06:38:33 -0600

    fixed menu bar: added link to brand back to home page.  //nt

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -11,7 +11,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">J. Allen Imports</a>
+      <a class="navbar-brand" href="/">J. Allen Imports</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
