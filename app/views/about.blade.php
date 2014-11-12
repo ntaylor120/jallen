fb620ca4088336cff68018a4056ae63910121e55
nancy.taylor <ntaylor120@gmail.com>
2014-11-12 17:19:07 -0600

    updated css and about page.  //nt

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -11,7 +11,7 @@
       <div class="row-fluid">
           <header class="span8 hero-unit"><!--Hero Unit Header starts here!-->
             <h1 class=>J. Allen Imports </h1>
-                <p >Importers of spirits made with all natural Quinoa</p>
+                <p >Importers of spirits made with all natural Ingredient X</p>
                     
           </header>   <!--Hero Unit Header stops here!--> 
       </div>
@@ -19,36 +19,25 @@
     <p>&nbsp;</p>
 
   
-      <div class="span2 pull-left">
-        <img src="img/Temuco Quinoa.jpg" alt="Quinoa Plant" class="img-polaroid">    
-      </div>
-    </div>
-
-    <div class="span1 pull-left">
-        <p>&nbsp;</p>
-    </div>
+     
         
        <div class="span6 pull-left">
-        <h3>About Quinoa</h3>
-        <p>Quinoa is an ancient grain that is organically grown in mostly in South America with high concentrations of Calcium, 
-            Phosphorus, and Iron and is replacing Wheat as the number one grain worldwide.  Quinoa also has a higher Protein content
-             than any of the other grains, as well as being Gluten-free.</p>
+        <h3>About Ingredient X</h3>
+        <p>Ingredient X (this is where I will describe what the ingredient is...... yada yada yada.. blah blah blah...)</p>
         </div>
 
         <p>&nbsp;</p>
          <p>&nbsp;</p>
            
    
-      <div class="span3 pull-left">
-        <img src="img/Rainbow Quinoa.jpg" alt="Rainbow Quinoa" class="img-polaroid">    
-      </div>
+      
         
          <div class="span6 pull-left">
         <h3>About Brand X</h3>
-        <p>Brand X is the finest tasting Vodka being manufactured in the wold made form Quinoa. 
-         This gives it a uniquely delicious flavor.  Quinoa imparts a slightly sweet, nutty aroma and 
+        <p>Brand X is the finest tasting Vodka being manufactured in the world made from Ingredient X. 
+         This gives it a uniquely delicious flavor.  Ingredient X imparts a slightly sweet, nutty aroma and 
          flavor to the spirit, while the Wheat gives it structure and a delayed attack at room temperature. 
-          When chilled, the Wheat takes a back seat to the viscous and nutty, almost mineral, quality of the Quinoa.</p>
+          When chilled, the Wheat takes a back seat to the viscous and nutty, almost mineral, quality of the Ingredient X.</p>
         </div>
     
       
