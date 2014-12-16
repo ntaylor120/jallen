9a31d6f837018160c7c1bd242bbd2d8a5d26213f
nancy.taylor <ntaylor120@gmail.com>
2014-12-16 05:54:22 -0600

    changed seeding for testing.  //nt

--- app/database/seeds/CommentsSeeder.php
+++ app/database/seeds/CommentsSeeder.php
@@ -14,7 +14,7 @@ class CommentsSeeder extends Seeder {
 //call uses table seeder class
   $this->call('UsersTableSeeder');
         //this message shown in your terminal after running db:seed command
-        $this->command->info("Users table seeded :)");
+        $this->command->info("review table seeded :)");
        }
 
 }
@@ -24,22 +24,22 @@ class UsersTableSeeder extends Seeder {
        public function run()
        {
          //delete users table records
-         DB::table('comments')->delete();
+         DB::table('reviews')->delete();
          //insert some dummy records
-         DB::table('comments')->insert(array(
+         DB::table('reviews')->insert(array(
             
 
-array('comments'=>'yummy', 'recipe_id' => '1'),
-array('comments'=>'too sweet, left out sugar', 'recipe_id' => '2'),
-array('comments'=>'not sweet enought, added more sugar', 'recipe_id' => '2'),
-array('comments'=>'just right amount of sugar', 'recipe_id' => '2'),
-array('comments'=>'needed more vodka', 'recipe_id' => '3'),
-array('comments'=>'perfect!', 'recipe_id' => '4'),
-array('comments'=>'love it', 'recipe_id' => '5'),
-array('comments'=>'excellent!', 'recipe_id' => '6'),
-array('comments'=>'made a double batch!', 'recipe_id' => '7'),
-array('comments'=>'made a triple batch', 'recipe_id' => '8'),
-array('comments'=>'will make again, excellent rcipe', 'recipe_id' => '9'),
+array('review'=>'yummy', 'recipe_id' => '1'),
+array('review'=>'too sweet, left out sugar', 'recipe_id' => '2'),
+array('review'=>'not sweet enought, added more sugar', 'recipe_id' => '2'),
+array('review'=>'just right amount of sugar', 'recipe_id' => '2'),
+array('review'=>'needed more vodka', 'recipe_id' => '3'),
+array('review'=>'perfect!', 'recipe_id' => '4'),
+array('review'=>'love it', 'recipe_id' => '5'),
+array('review'=>'excellent!', 'recipe_id' => '6'),
+array('review'=>'made a double batch!', 'recipe_id' => '7'),
+array('review'=>'made a triple batch', 'recipe_id' => '8'),
+array('review'=>'will make again, excellent rcipe', 'recipe_id' => '9'),
 
           ));
        }
